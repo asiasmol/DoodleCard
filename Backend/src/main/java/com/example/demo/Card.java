1429f31a651934ce73890e9889063b1356d31465
asiasmol <joanna.smolinska.cool@gmail.com>
2024-01-03 21:13:06 +0100

    Implementation of Card Entity

---
+++ Backend/src/main/java/com/example/demo/Card.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="cards")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+}
